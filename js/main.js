741c78e109504d0c634ede7541e76d884fc70023
coder-tanvir <tanvirrahmankhan@gmail.com>
2023-01-04 03:12:44 -0800

    Added FAQ section

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 $(document).ready(function(){
 
-    $('.fa-bars').click(function(){
-        $(this).toggleClass('fa-times');
-        $('.navbar').toggleClass('nav-toggle');
-    });
-
-
+$('.accordion-header').click(function(){
+    $( '.accordion-body').slideUp();
+    $(this).next('.accordion-body').slideDown();
+    $('.accordion .accordion-header span').text('+');
+    $(this).children('span').text('-');  
 })
+
+});
