8ec5f91152d9c836263f2a8e9cab5cef6abf18b0
coder-tanvir <tanvirrahmankhan@gmail.com>
2023-01-04 03:26:33 -0800

    message

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
 
 $('.accordion-header').click(function(){
-    $( '.accordion-body').slideUp();
+    $( '.accordion-body').slideUp(); 
     $(this).next('.accordion-body').slideDown();
     $('.accordion .accordion-header span').text('+');
-    $(this).children('span').text('-');  
+    $(this).children('span').text('-'); 
+    
 })
 
 });
